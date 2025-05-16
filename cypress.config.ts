2d0dbba4d9f507f55d1c2a6eaee9e1be4913787e
Safsaf <zaafourisafiya@gmail.com>
2025-05-16 16:57:03 +0100

    Add Cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,15 +1,13 @@
-import { defineConfig } from 'cypress'
+// cypress.config.ts
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
-    supportFile: false, // ou ton fichier support ex: 'cypress/support/e2e.ts'
-    specPattern: 'cypress/e2e/**/*.cy.{ts,js}',
-
     setupNodeEvents(on, config) {
-      // configure les événements si nécessaire
-      return config
+      // implement node event listeners here
     },
   },
-})
+});
+
 
