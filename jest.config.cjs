d06825afd6a9bb55f7d70a4928a92a036cf2107a
Safsaf <zaafourisafiya@gmail.com>
2025-05-30 21:32:45 +0100

    lancer les tests pour ma pipeline CI/CD

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,7 +1,11 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 
 module.exports = {
-  testMatch: ["**/tests/unit/**/*.test.[jt]s?(x)"],
+  testMatch: [
+    "**/tests/unit/**/*.test.[jt]s?(x)",
+    "**/tests/integration/**/*.test.[jt]s?(x)",
+    "**/tests/e2e/**/*.spec.[jt]s?(x)" // optionnel si tu veux tester e2e aussi
+  ],
+  testPathIgnorePatterns: ["/node_modules/"],
 };
-
 
